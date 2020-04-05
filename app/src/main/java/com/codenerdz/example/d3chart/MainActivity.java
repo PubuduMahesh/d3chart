a295f95fc9d49ce918a5371fafe0460303c0dbdd
PMeththananda <Pubudu.Meththananda@cambio.lk>
2020-04-05 10:10:40 +0530

    initial commit

---
+++ app/src/main/java/com/codenerdz/example/d3chart/MainActivity.java
@@ -0,0 +1,14 @@
+package com.codenerdz.example.d3chart;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
